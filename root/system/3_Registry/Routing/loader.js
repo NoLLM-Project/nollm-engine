20d7ed46c0e5e3584ba82753c763deeb42c0983e
NoLLM-Project <nollm-keeper@proton.me>
2026-06-23 09:27:23 -0400

    Add files via upload

--- root/system/3_Registry/Routing/loader.js
+++ root/system/3_Registry/Routing/loader.js
@@ -1,10 +1,10 @@
-import { loadJson } from "../../utils/load_json.js"; // or correct absolute path
+import { loadJson } from "../../../utils/load_json.js";
 
 export async function loadRoutingRegistry() {
   const basePath = "/3_Registry/Routing/transitions";
 
   // Load schema via fetch, not import
-  const routingSchema = await loadJson("/3_Registry/Routing/routing_schema.json");
+  const routingSchema = await loadJson("./3_Registry/Routing/routing_schema.json");
 
   const allTransitions = readJsonFilesRecursively(basePath);
   const transitions = allTransitions.flat();
