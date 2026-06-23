907b1b250c37e1e3ff0c8d67e924362e540d112b
NoLLM-Project <nollm-keeper@proton.me>
2026-06-23 08:20:57 -0400

    Add files via upload

--- root/system/5_Function/validators/load_metadata_invariants.js
+++ root/system/5_Function/validators/load_metadata_invariants.js
@@ -1,6 +1,6 @@
 // system/5_Function/validators/load_metadata_invariants.js
 
-import { loadJson } from "../../utils/load_json.js";
+import { loadJson } from "../../../utils/loadJson.js";
 
 export async function loadMetadataInvariants() {
     return await loadJson("../../3_Registry/Metadata/metadata_invariants.json");
--- root/system/5_Function/validators/load_sku_invariants.js
+++ root/system/5_Function/validators/load_sku_invariants.js
@@ -1,4 +1,4 @@
-import { loadJson } from "../../utils/load_json.js";
+import { loadJson } from "../../../utils/loadJson.js";
 
 export async function loadSKUInvariants() {
     return await loadJson("../../3_Registry/SKUs/sku_invariants.json");
